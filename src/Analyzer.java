3343c2fbb98f806c1c84bedf582db8b17c84cf60
YanaPotemkina <potemkinayana@yandex.ru>
2022-02-02 23:10:16 +0300

    Bug fix

--- src/Analyzer.java
+++ src/Analyzer.java
@@ -1,14 +1,20 @@
 import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Collections;
 
 
 public class Analyzer {
 
-    private static final String ALPHABET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZабвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ .,!:;?-%0123456789";
+    public static final String ALPHABET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZабвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ .,!:;?%0123456789";
+    public static final ArrayList<Character> CRYPTO_CHARACTERS = new ArrayList<>();
+    public static final ArrayList<Character> CRYPTO_CHARACTERS_WITH_KEY = new ArrayList<>();
     private static final String INPUT = "Введите полный путь к файлу:";
+    public static final ArrayList<Character> NEW_CRYPTO_TEXT = new ArrayList<>();
     private static final BufferedReader READER = new BufferedReader(new InputStreamReader(System.in));
     private static final String START_MESSAGE = """
             Введите число для выбора режима программы:
@@ -23,14 +29,15 @@ public class Analyzer {
     }
 
     private static void getStart() throws IOException {
+        createAlphabet();
         System.out.println(START_MESSAGE);
         String choice = READER.readLine();
         while (!choice.equals("4")) {
             switch (choice) {
-                case "1" -> encrypt();
-                case "2" -> decrypt();
+                case "1" -> cryptoTextWithKey();
+                case "2" -> unCryptoTextWithKey();
                 case "3" -> brutForce();
-                default -> System.out.println("Неккоректный ввод данных. Введите число от 1 до 4");
+                default -> System.out.println("Неккоректный ввод данных. Введите число от 1 до 4.\n");
             }
             System.out.println(START_MESSAGE);
             choice = READER.readLine();
@@ -38,102 +45,79 @@ public class Analyzer {
         System.out.println("Выход из программы");
     }
 
-    private static void encrypt() throws IOException {
+    public static void cryptoTextWithKey() throws IOException {
+        createCryptoAlphabet();
         System.out.println(INPUT);
         String filePath = READER.readLine();
-        String userText = getFile(filePath);
         System.out.println("Введите ключ:");
         int key = Integer.parseInt(READER.readLine());
-        writeNewTextToFile(encryptWithKey(userText, key), filePath, "_encrypted");
+        Collections.rotate(CRYPTO_CHARACTERS_WITH_KEY, -key);
+        StringBuilder cryptText = crypt(filePath);
+        writeNewTextToFile(cryptText, filePath, "_encrypted");
+        CRYPTO_CHARACTERS_WITH_KEY.clear();
     }
 
-    private static StringBuilder encryptWithKey(String userText, int key) {
+
+    public static void unCryptoTextWithKey() throws IOException {
+        createCryptoAlphabet();
+        System.out.println(INPUT);
+        String filePath = READER.readLine();
+        System.out.println("Введите ключ:");
+        int key = Integer.parseInt(READER.readLine());
+        Collections.rotate(CRYPTO_CHARACTERS_WITH_KEY, key);
+        StringBuilder cryptText = crypt(filePath);
+        writeNewTextToFile(cryptText, filePath, "_decrypted");
+        CRYPTO_CHARACTERS_WITH_KEY.clear();
+    }
+
+
+    public static StringBuilder crypt(String path) throws IOException {
         StringBuilder encryptText = new StringBuilder();
-        for (int i = 0; i < userText.length(); i++) {
-            char charText = userText.charAt(i);
-            int charIndex = ALPHABET.indexOf(charText);
-            if (charIndex == -1) {
-                encryptText.append(charText);
-            } else {
-                int newCharIndex = (charIndex + key) % ALPHABET.length();
-                char encryptChar = ALPHABET.charAt(newCharIndex);
-                encryptText.append(encryptChar);
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                char[] chars = line.toCharArray();
+                for (char aChar : chars) {
+                    int cryptoIndex = CRYPTO_CHARACTERS.indexOf(aChar);
+                    if (cryptoIndex == -1) {
+                        encryptText.append(aChar);
+                    } else {
+                        char messageIndex = CRYPTO_CHARACTERS_WITH_KEY.get(cryptoIndex);
+                        encryptText.append(messageIndex);
+                    }
+                }
+                encryptText.append("\n");
             }
         }
         return encryptText;
     }
 
-    private static void decrypt() throws IOException {
-        System.out.println(INPUT);
-        String filePath = READER.readLine();
-        String userText = getFile(filePath);
-        System.out.println("Введите ключ:");
-        int key = Integer.parseInt(READER.readLine());
-        writeNewTextToFile(decryptWithKey(userText, key), filePath, "_decrypted");
-    }
-
-    private static StringBuilder decryptWithKey(String userText, int key) {
-        StringBuilder decryptText = new StringBuilder();
-        for (int i = 0; i < userText.length(); i++) {
-            char charText = userText.charAt(i);
-            int charIndex = ALPHABET.indexOf(charText);
-            if (charIndex == -1) {
-                decryptText.append(charText);
-            } else {
-                int newCharIndex = Math.abs((charIndex - key) % ALPHABET.length());
-                char encryptChar = ALPHABET.charAt(newCharIndex);
-                decryptText.append(encryptChar);
-            }
-        }
-        return decryptText;
-    }
-
     private static void brutForce() throws IOException {
+        createCryptoAlphabet();
         System.out.println(INPUT);
         String filePath = READER.readLine();
-        String userText = getFile(filePath);
         for (int key = 0; key < ALPHABET.length(); key++) {
-            StringBuilder decryptedText = decryptWithKey(userText, key);
+            Collections.rotate(CRYPTO_CHARACTERS_WITH_KEY, 1);
+            StringBuilder decryptedText = crypt(filePath);
             boolean isValid = isValidText(decryptedText);
             if (isValid) {
                 writeNewTextToFile(decryptedText, filePath, "_brut");
                 break;
             }
         }
+        CRYPTO_CHARACTERS_WITH_KEY.clear();
     }
 
     private static boolean isValidText(StringBuilder decryptedText) {
-        char[] chars = decryptedText.toString().toCharArray();
-        int points = 0;
-        int comma = 0;
-        int exclamation = 0;
-        for (int i = 0; i < chars.length - 1; i++) {
-            if (chars[i] == '.' && chars[i + 1] == ' ') {
-                points++;
-            } else if (chars[i] == ',' && chars[i + 1] == ' ') {
-                comma++;
-            } else if (chars[i] == '!' && chars[i + 1] == ' ') {
-                exclamation++;
-            } else if ((chars[i] == ' ' && chars[i + 1] == 'Ъ') || (chars[i] == ' ' && chars[i + 1] == 'Ь')) {
+        String[] strings = decryptedText.toString().split(" ");
+        for (String string : strings) {
+            if (string.length() > 40) {
                 return false;
             }
-            if (points > 20 && comma > 30 && exclamation > 10) {
-                return true;
-            }
         }
-        return false;
+        return true;
     }
 
-    private static String getFile(String filePath) {
-        Path path = Path.of(filePath);
-        try {
-            byte[] bytes = Files.readAllBytes(path);
-            return new String(bytes);
-        } catch (IOException e) {
-            System.out.println("Ошибка чтения файла.\n");
-        }
-        return filePath;
-    }
 
     private static void writeNewTextToFile(StringBuilder encryptText, String filePath, String suffix) {
         try {
@@ -145,6 +129,18 @@ public class Analyzer {
             System.out.println("Программа выполнена успешна!\nПуть к новому файлу: " + fileBeforeDot + suffix + fileAfterDot + "\n");
         } catch (IOException e) {
             System.out.println("Ошибка записи файла.\n");
+        }
+    }
+
+    public static void createAlphabet() {
+        for (int i = 0; i < ALPHABET.length(); i++) {
+            CRYPTO_CHARACTERS.add(i, ALPHABET.charAt(i));
+        }
+    }
+
+    public static void createCryptoAlphabet() {
+        for (int i = 0; i < CRYPTO_CHARACTERS.size(); i++) {
+            CRYPTO_CHARACTERS_WITH_KEY.add(i, CRYPTO_CHARACTERS.get(i));
         }
     }
 }
